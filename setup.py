61db8ce0ab63f7fd9beef48437c879257b696d3f
drwoodard <danwoodard+gh@gmail.com>
2024-09-22 01:37:20 -0400

    Fixed bug in setup.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='music_services',
     version='0.1',
     packages=find_packages(),
-    install_requires=['requests, python-dotenv'],
+    install_requires=['requests', 'python-dotenv'],
     author='Dan Woodard',
     description='A package to interact with Spotify, Discogs, Last.FM and MusicBrainz APIs.',
 )
